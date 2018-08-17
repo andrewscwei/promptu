457eea5c7a74bd62165aae53caf5a72bab15a8df
Andrew Wei <andrewscwei@gmail.com>
2018-08-16 18:57:43 -0700

    Add fonts

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const align = require(`./lib/align`);
 const box = require(`./lib/box`);
+const fonts = require(`./lib/fonts`);
 const media = require(`./lib/media`);
 const normalize = require(`./lib/normalize`);
 
 module.exports = {
   align,
   box,
+  fonts,
   media,
   normalize,
 };
