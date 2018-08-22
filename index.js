4e800c68539a39a4963d4bbd6a0a9539f2567183
Andrew Wei <andrewscwei@gmail.com>
2018-08-21 22:11:36 -0700

    Fix imports

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const align = require(`./lib/align`);
 const container = require(`./lib/container`);
 const media = require(`./lib/media`);
 const normalize = require(`./lib/normalize`);
-const typography = require(`./lib/font`);
+const typography = require(`./lib/typography`);
 const utils = require(`./utils`);
 
 module.exports = {
