c4f4f1d7b08e073dd86de6c7adc7d6774d2ef998
Andrew Wei <andrewscwei@gmail.com>
2019-06-19 18:37:36 -0700

    Forgot to export media mixins

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as align from './core/align';
 import * as container from './core/container';
+import * as media from './core/media';
 import normalize from './core/normalize';
 import * as typography from './core/typography';
 import * as utils from './core/utils';
 
 export * from './types';
-export { align, container, normalize, typography, utils };
+export { align, container, media, normalize, typography, utils };
