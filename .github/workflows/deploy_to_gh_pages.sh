ac6d425e2881cd7e5257eca9dabc1f6197dad02c
Andrew Wei <andrewscwei@gmail.com>
2020-10-18 16:10:21 -0700

    Test GitHub pages

--- .github/workflows/deploy_to_gh_pages.sh
+++ .github/workflows/deploy_to_gh_pages.sh
@@ -3,10 +3,13 @@
 # Required environment variables:
 # - GH_USER: Name of GitHub user to use for the commit
 # - GH_PAGES_DIR: Directory (relative to project root) to deploy to GitHub Pages
+# - GH_REPO: The owner and repository name to commit to.
 
 __GH_PAGES_DIR__=${GH_PAGES_DIR:-.gh-pages}
 __GH_USER__=${GH_USER:-$GITHUB_ACTOR}
-__ORIGIN_URL__=`git config --get remote.origin.url`
+__GH_REPO__=${GH_REPO:-$GITHUB_REPOSITORY}
+# __ORIGIN_URL__=`git config --get remote.origin.url`
+__ORIGIN_URL__="https://$__GH_USER__:$GH_TOKEN@github.com/$__GH_REPO__.git"
 
 if [ `git branch | grep gh-pages` ]; then
   git branch -D gh-pages
@@ -21,12 +24,10 @@ mv ${__GH_PAGES_DIR__}/* .
 rm -R ${__GH_PAGES_DIR__}/
 
 # Push to gh-pages.
-git config user.name "$__GH_USER__"
-git config user.email "$__GH_USER__@users.noreply.github.com"
+git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
+git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
 git add -fA
 git commit --allow-empty -m "[SKIP CI] $(git log -1 --pretty=%B)"
 git push -f $__ORIGIN_URL__ gh-pages
 
-echo $__GH_USER__
-echo $GITHUB_ACTOR
 echo "Successfully published docs to GitHub Pages"
