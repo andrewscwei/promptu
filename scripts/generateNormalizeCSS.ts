c38c4a6ed0c7a8f0f90997a388c7e722e84b8ca3
Andrew Wei <andrewscwei@gmail.com>
2023-04-16 11:36:53 -0700

    Upgrade dependencies and CI/CD scripts

--- scripts/generateNormalizeCSS.ts
+++ scripts/generateNormalizeCSS.ts
@@ -1,10 +1,10 @@
-import fs from 'fs';
-import path from 'path';
-import normalize from '../src/core/normalize';
+import fs from 'fs'
+import path from 'path'
+import normalize from '../src/core/normalize'
 
-const pwd = path.join(__dirname, '../');
-const outDir = path.join(pwd, 'lib');
-const css = normalize();
+const pwd = path.join(__dirname, '../')
+const outDir = path.join(pwd, 'lib')
+const css = normalize()
 
-fs.mkdirSync(outDir);
-fs.writeFileSync(path.join(outDir, 'normalize.css'), css);
+fs.mkdirSync(outDir)
+fs.writeFileSync(path.join(outDir, 'normalize.css'), css)
