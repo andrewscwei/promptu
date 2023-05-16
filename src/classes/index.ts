93d8d673fb18b44f9ac64c98d1d2cf37e359abb9
andrewscwei <andrewscwei@users.noreply.github.com>
2023-05-16 00:55:13 +0000

    [SKIP CI] Update normalize styles

--- src/classes/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './align'
-export * from './container'
-export * from './images'
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-/** @license promptu
- * © Andrew Wei
- * This source code is licensed under the MIT license found in the LICENSE file
- * in the root directory of this source tree.
- */
-
-export * as classes from './classes'
-export * from './core'
-export * as types from './types'
